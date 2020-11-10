f63307474864ec90f215d89880f9668b2c2abdc2
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2020-11-10 19:49:23 +0300

    Create raschet.cpp

---
+++ raschet.cpp
@@ -0,0 +1,19 @@
+#include "header.h"
+
+void raschet() {
+	Vect vect1, vect2, vect3;
+	CRat a;
+	vect1 = input();
+	vect2 = input();
+	cout << "\n______________________________________________\n" << endl;
+	cout << "Summa vectorov= ";
+	vect3 = vect1 + vect2;
+	vect3.print();
+	vect3 = vect1 - vect2;
+	cout << "\nRaznost' vectorov= ";
+	vect3.print();
+	a = vect1 * vect2;
+	cout << "\nScalProizv vectorov= ";
+	a.print();
+	cout << "\n______________________________________________\n\n" << endl;
+};
