8fd0a25dad6b681cf2d540eb17ad852cee49d3d2
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2020-11-10 14:57:58 +0300

    Create Vect.h

---
+++ Vect.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm> 
+#include <stdio.h>
+#include <math.h>
+#include "CRat.h"
+using namespace std;
+
+
+class Vect {
+private:
+	CRat a[LENGTH];
+public:
+	Vect() {
+		for (int i = 0; i < LENGTH; i++) {
+			CRat m;
+			a[i] = m;
+		};
+	}
+	Vect(const CRat *b) {
+		for (int i = 0; i < LENGTH; i++) {
+			a[i] = b[i];
+		}
+	};
+	Vect& operator=(const Vect& b);
+	Vect operator+(const Vect& b);
+  Vect operator-(const Vect& b);
+	Vect Conjugate();
+	CRat operator*(Vect& b);
+	void print();
+};
+Vect input(void);
+void raschet(void);
+void autotest(void);
